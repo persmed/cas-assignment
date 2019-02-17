319521b4e5c2184cc4873a458336ca2368ac6da4
Iwan Paolucci <iwan.paolucci@artorg.unibe.ch>
2019-02-17 22:35:28 +0100

    script to test the installation

---
+++ cas/test_installation.py
@@ -0,0 +1,16 @@
+import numpy as np
+print('Numpy version:\t\t', np.__version__)
+
+import vtk
+print('VTK version:\t\t', '{0}.{1}'.format(vtk.VTK_MAJOR_VERSION, vtk.VTK_MINOR_VERSION))
+
+import SimpleITK as sitk
+print('SimpleITK version:\t', '{0}.{1}'.format(sitk.SITK_ITK_VERSION_MAJOR, sitk.SITK_ITK_VERSION_MINOR))
+
+import matplotlib
+print('Matplotlib version:\t', matplotlib.__version__)
+
+import transformations
+print('Transformations version:\t', transformations.__version__)
+
+print('Your environment is ready...')
