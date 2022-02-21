0032db08a779f2ba1cb8e6cb55fd2f3e39d29063
Kate <kate.gerber@sitem.unibe.ch>
2022-02-21 17:46:55 +0100

    clarifying calibration variables

--- assignments/toolcalibration/calibration.py
+++ assignments/toolcalibration/calibration.py
@@ -6,7 +6,7 @@ def pivot_calibration(transforms):
     Keyword arguments:
     transforms -- A list with 4x4 transformation matrices,  (Fi)
     returns -- A vector p_t, which is the offset from any transform, Fi, to the pivot point
-            -- The calibration matrix T (p_t in a 4X4 matrix form)    """
+            -- The calibration matrix T (p_t in a 4X4 matrix form)  """
 
     ## TODO: Implement pivot calibration as discussed in the lecture
     p_t = np.zeros((3, 1))
@@ -21,6 +21,7 @@ def calibration_device_calibration(camera_T_reference, camera_T_tracker, referen
     camera_T_tracker -- Transformation from the tool tracker to the camera
     reference_P_pivot -- A pivot on the calibration device reference (rigid body) where the tip of
                          the instrument is located for calibration
+    returns -- The calibration matrix, tracker_T_pivot: i.e the tool tip location (reference_P_pivot) relative to the tracked tool (camera_T_tracker)
     """
     
     ## TODO: Implement a calibration method which uses a calibration device
